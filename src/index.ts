11144a2c2512ace1748f5640a6594c49fbcf7eb1
almeida <hello@eualmeida.com.br>
2026-02-13 19:13:41 -0300

    feat(index.ts): exporting errors, REST, types and versions

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+export { REST } from './client';
+
+export {
+	RESTOptions,
+	RetryOptions,
+	FetchOptions,
+	HTTPMethodLike
+} from './types';
+
+export * from './errors';
+
+export { version, API_VERSION } from './version';
---
+++ src/version.ts
@@ -0,0 +1,10 @@
+// AUTO-GENERATED — DO NOT EDIT
+
+/**
+ * Current version of [@rewritejs/rest](https://www.npmjs.com/package/@rewritejs/rest).
+ */
+export const version = '1.0.0' as const;
+/**
+ * Current version of the Rewrite API.
+ */
+export const API_VERSION = '1' as const;
